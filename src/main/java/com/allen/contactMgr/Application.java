653704048dbd70931f87b2d6f4572c33edc70171
Allen Lucke <allenlucke@gmail.com>
2020-05-06 13:01:20 -0500

    added builder class

--- src/main/java/com/allen/contactMgr/Application.java
+++ src/main/java/com/allen/contactMgr/Application.java
@@ -1,7 +1,36 @@
 package com.allen.contactMgr;
 
-public class Application {
-    public static void main(String[] args) {
+import com.allen.contactMgr.model.Contact;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
+
+public class Application {
+    //Reusable reference to a SessionFactory
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        //Create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new  MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+    public static void main(String[] args) {
+        Contact contact = new Contact.ContactBuilder("Tom", "Jones")
+                .withEmail("tj@tj.com")
+                .withPhone(8882223333L)
+                .build();
+        //Opens session
+        Session session = sessionFactory.openSession();
+        //Begins a transaction
+        session.beginTransaction();
+        //Uses session to save contact
+        session.save(contact);
+        //Commits the transaction
+        session.getTransaction().commit();
+        //Closes the session
+        session.close();
     }
 }
